dd320d39244955194075d3ef4eaaebe1ed398c82
Rafael Pinto da Silva <57225298+Rafael-doctom@users.noreply.github.com>
2020-11-17 09:09:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var n1 = Number(prompt('Digite um valor:'))
+var n2 = Number(prompt('Digite outro valor:'))
+confirm(`Você digitou ${n1} + ${n2}. Você tem certeza?`)
+var total = n1 + n2 
+alert(`A soma dos valores acima é igual: ${total}`)
